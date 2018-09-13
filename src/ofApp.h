8893da4547c46667e7235aef3c503ec7e4b361e5
chechojazz <sergio.giraldo@upf.edu>
2018-09-13 14:00:20 +0100

    add recordings, save and load models

--- src/ofApp.h
+++ src/ofApp.h
@@ -1,12 +1,13 @@
 #pragma once
 
-#include "ofMain.h"
+//#include "ofMain.h"
 #include "ofxOsc.h"
 #include "logisticRegression.hpp"
 #include "SVM.hpp"
 #include "NeuralNetworks.hpp"
 #include "utils.hpp"
-
+#include <string>
+#include <opencv2/opencv.hpp>
 
 class ofApp : public ofBaseApp{
     
@@ -19,11 +20,8 @@ public:
     ofxOscReceiver receiver;
     ofxOscReceiver receiverRecord;
     
-    ofFile firstModel;
-    ofFile secondModel;
-    ofFile thirdModel;
-    ofFile fourthModel;
-    ofFile fifthModel;
+    ofFile firstModel,secondModel,thirdModel,fourthModel,fifthModel;
+    ofFile firstModelWeights,secondModelWeights,thirdModelWeights,fourthModelWeights,fifthModelWeights;
         
     float peak;
     float freq;
@@ -41,5 +39,10 @@ public:
     void windowResized(int w, int h);
     void dragEvent(ofDragInfo dragInfo);
     void gotMessage(ofMessage msg);
+    
+    //bool sortColorFunction (ofColor i,ofColor j);
+    vector<ofImage>loadedImages;
+    vector<ofImage>processedImages;
+    string originalFileExtension;
     
 };
